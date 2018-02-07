0319b4bfec30fb0e3ed99d11f6ad068495176782
Vlad Romanenko <vlad.romanenko@hotmail.com>
2018-02-07 22:04:53 +0000

    Use map variable to configure AMIs.

--- example.tf
+++ example.tf
@@ -2,6 +2,14 @@
 variable "access_key" {}
 variable "secret_key" {}
 
+variable "amis" {
+  type = "map"
+  default = {
+    "amazon-linux" = "ami-403e2524"   # Amazon Linux AMI 2017.09.1
+    "amazon-linux-2" = "ami-6d263d09" # Amazon Linux 2 LTS Candidate AMI 2017.12.0
+  }
+}
+
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
@@ -18,8 +26,7 @@ resource "aws_s3_bucket" "example" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-6d263d09" # Amazon Linux 2 LTS Candidate AMI 2017.12.0
-#  ami           = "ami-403e2524" # Amazon Linux AMI 2017.09.1
+  ami           = "${var.amis["amazon-linux-2"]}" 
   instance_type = "t2.micro"
 
   # Tells Terraform that this EC2 instance must be created only after the
