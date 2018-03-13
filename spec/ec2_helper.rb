ddf1048f4bd2539d9c4222e8bf2501818dcb127f
Vlad Romanenko <vlad.romanenko@hotmail.com>
2018-03-13 10:54:06 +0000

    Workaround for terminated instance failing the test.

---
+++ spec/ec2_helper.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'awspec'
+require 'aws-sdk'
+
+def ec2_instances_named(name_tag)
+  filters = [
+    { name: 'instance-state-name', values: ['pending', 'running', 'stopped' ] },
+    { name: 'tag:Name', values: [name_tag] }
+  ]
+  selected_ec2_instances(filters).map { |instance| 
+    ec2(instance.instance_id)
+  }
+end
+
+def selected_ec2_instances(filters)
+  response = ec2_client.describe_instances(filters: filters)
+  if response.reservations.nil? || response.reservations.empty?
+    []
+  else
+    response.reservations.map { |reservation| reservation.instances }.flatten
+  end
+end
+
+def ec2_client
+  Aws::EC2::Client.new(region: 'eu-west-2')
+end
--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
+require 'ec2_helper'
 
 describe vpc('my-vpc') do
   it { should exist }
   its(:cidr_block) { should eq '10.0.0.0/16' }
 end
 
-describe ec2('example') do
-  it { should exist }
-  it { should be_running }
-  it { should belong_to_vpc('my-vpc') }
+#Below fails when an instace with same name is terminated:
+# https://github.com/k1LoW/awspec/issues/155
+#describe ec2('example') do
+#  it { should exist }
+#  it { should be_running }
+#  it { should belong_to_vpc('my-vpc') }
+#end
+
+describe ec2_instances_named('example') do
+  its(:size) { should eq 1 }
+  #it { should have(1).items } # This fails with: undefined method `have' for #<RSpec::ExampleGroups::Ec2I05a41c133451f970d:0x007f99a8b72750>
+  it { is_expected.to all(exist) }
+  it { is_expected.to all(be_running) }
 end
